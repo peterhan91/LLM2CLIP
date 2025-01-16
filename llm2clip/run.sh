a727fb9be5f7683248eae703b1cb57a60cdc6e64
hwq <weiquanhhh@gmail.com>
2025-01-16 08:45:47 +0000

    add training data

--- llm2clip/run.sh
+++ llm2clip/run.sh
@@ -1,26 +1,22 @@
-MODEL=EVA02-CLIP-L-14-336
+MODEL=EVA02-CLIP-L-14
 PRETRAINED=eva_clip
-python -m torch.distributed.launch --nproc_per_node=8 \
+python -m torch.distributed.launch --nproc_per_node=2 \
 	--use_env training/main.py \
         --enable-deepspeed \
         --grad-checkpointing \
         --name="T_vitl336_Rcc12mR_Rcc3m_4ep" \
         --save-frequency 1  \
         --zeroshot-frequency 1 \
-        --report-to="tensorboard, wandb" \
+        --report-to="" \
         --wandb-project-name="LLM2CLIP" \
         --wandb-notes="EVA02-CLIP-L-14-336" \
-        --train-data-list "data/cc3m/cc3m-train-{00..0287}.tar;data/cc12m/cc12m-train-{00..1001}.tar" \
-        --train-num-samples-list 2873538  10000225 \
-        --eval-data-file=training/eval_datasets.yaml \
-        --imagenet-val=data/eval_data/imagenet/val.zip \
-        --imagenet-val-text=data/eval_data/imagenet/val_map.txt \
-        --imagenet-classname-feautres data/eval_data/imagenet/im_classname_llm_features.dpt \
+        --train-data-list "/home/aiscuser/LLM2CLIP/llm2clip/data/cc3m/{00..00004}.tar" \
+        --train-num-samples-list 2873538  \
         --pretrained=${PRETRAINED} \
         --dataset-resampled \
         --precision "fp16" \
         --warmup 0 \
-        --batch-size=512 \
+        --batch-size=16 \
         --eval-batch-size=1024 \
         --log-every-n-steps 50 \
         --epochs=20 \
@@ -35,7 +31,7 @@ python -m torch.distributed.launch --nproc_per_node=8 \
         --visual-ld=0.85 \
         --grad-clip-norm=5.0 \
         --smoothing=0. \
-        --workers=8 \
+        --workers=1 \
         --model=${MODEL} \
         --seed 4096 \
         --gather-with-grad \
